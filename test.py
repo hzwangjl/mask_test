c1d72d7b4a6ce38c3d55e815a52ac294d97ec465
hzwangjialei <hzwangjialei@corp.netease.com>
2019-07-01 21:53:41 +0800

    detect contour

--- test.py
+++ test.py
@@ -19,7 +19,16 @@ img = y1.astype('uint8')*255
 y1 = ndimage.binary_fill_holes(y1)
 y1_img = ndimage.binary_opening(y1, iterations=10)
 y1_img = y1_img.astype('uint8')*255
-cv2.imshow('im1', img)
+
+color = cv2.cvtColor(y1_img, cv2.COLOR_GRAY2BGR) 
+# ret, binary = cv2.threshold(y1_img,127,255,cv2.THRESH_BINARY)
+im, contours, hierarchy = cv2.findContours(y1_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(color, contours, -1, (0, 255, 0), 2)
+print((contours))
+# cv2.drawContours(color, contours[0], -1, (0,0,255), 3)  
+
+# cv2.imshow('im1', img)
 cv2.imshow('im2', y1_img)
+cv2.imshow('im3', color)
 cv2.waitKey(10000)
 
